ce281809e9e23d9e81f0c4f0dc3c04c46b7c0867
flores <mflores23-0287@cca.edu.ph>
2025-02-17 17:51:37 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	 <head>
+		<title> Java Script Form </title>
+	 </head>
+	 <body>
+		<h1> Student Record </h1>
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable= document
+				.getElementById("student_table");
+				
+			console.log(studentTable);
+		</script>
+	 </body>
+</html>
+	 
