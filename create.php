7a0a4a7c7d41b647e6818428f690c3dc15fbbeb4
flores <mflores23-0287@cca.edu.ph>
2025-02-17 19:25:30 +0800

    7yt

--- create.php
+++ create.php
@@ -2,4 +2,23 @@
 	echo "Hello World";
 	
 	var_dump($_POST);
+	
+?> 
+<br>
+<br>
+<br>
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "mariqt";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $database);
+
+	// Check connection
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connected successfully";
 ?>
