d0edde16ea5b2c4a1921327e3d233b57131fafa2
flores <mflores23-0287@cca.edu.ph>
2025-02-17 18:56:51 +0800

    dfsf

--- index.php
+++ index.php
@@ -45,6 +45,14 @@
 			inputSubmit.value="Create Record";
 			
 			row3col1.append(inputSubmit);
+			
+			const studentForm=document.createElement("form");
+			studentForm.appendChild(studentTable);
+			studentForm.action="create.php";
+			studentForm.method="Post";
+			
+			body = document.getElementsByTagName('body')[0];
+			body.append(studentForm);
 		</script>
 	 </body>
 </html>
