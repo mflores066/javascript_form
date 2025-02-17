75ed07b5f86204aaaa646b8d12b3987cde0af6c2
flores <mflores23-0287@cca.edu.ph>
2025-02-17 19:37:57 +0800

    etewtt

--- create.php
+++ create.php
@@ -2,7 +2,8 @@
 	echo "Hello World";
 	
 	var_dump($_POST);
-	
+	$lastname = $_POST['last_name'];
+	$firstname = $_POST['first_name'];
 ?> 
 <br>
 <br>
@@ -21,4 +22,16 @@
 	  die("Connection failed: " . $conn->connect_error);
 	}
 	echo "Connected successfully";
+
+	$sql = "INSERT INTO Students (Id, last_name, first_name)
+	VALUES ('', '$lastname', '$firstname')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "<br>";
+		echo "<br>";
+		echo "<br>";
+	  echo "New record created successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 ?>
