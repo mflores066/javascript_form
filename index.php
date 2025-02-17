827ad67034e7b9f65250218a0b8bd7df4a30f58e
flores <mflores23-0287@cca.edu.ph>
2025-02-17 18:34:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	 </head>
 	 <body>
 		<h1> Student Record </h1>
-		<table id="student_table">
+		<table id="student_table" border = 1>
 		</table>
 		
 		<script>
@@ -19,6 +19,27 @@
 			
 			row1col1.innerHTML = "Last Name";
 			row1col2.innerHTML = "First Name";
+			
+			const row2 = studentTable.insertRow();
+			const row2col1 = row2.insertCell();
+			const row2col2 = row2.insertCell();
+			
+			const inputLastName = document.createElement("input");
+			inputLastName.type="text";
+			inputLastName.placeholder="Insert Last Name";
+			
+			row2col1.append(inputLastName);
+			
+			const inputFirstName = document.createElement("input");
+			inputFirstName.type="text";
+			inputFirstName.placeholder="Insert First Name";
+			
+			row2col2.append(inputFirstName);
+			
+			const row3 = studentTable.insertRow();
+			const row3col1 = row3.insertCell();
+			row3col1.colSpan="2";
+			row3col1.innerHTML="TEST";
 		</script>
 	 </body>
 </html>
